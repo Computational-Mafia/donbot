3c3c36e28a85fa833d5243e957496c2489b6230b
Jordan Gunn <gunnjordanb@gmail.com>
2024-01-31 02:28:07 -0600

    cleanup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='donbot',
+    version='0.1.0',
+    description='automating the more trivial activities a game moderator does on mafiascum.net, and other stuff sprouting from that effort',
+    author='Jordan Gunn',
+    author_email='gunnjordanb@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: GPL-3.0',
+        'Programming Language :: Python :: 3'
+    ],
+)
