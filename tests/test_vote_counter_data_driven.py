f69c7d31dcef946e3abc5a88b5a1e95e41f3dcc4
Jordan Gunn <gunnjordanb@gmail.com>
2024-04-14 18:43:49 -0500

    basic test for integrity of data loader, fixes in the test using it

--- tests/test_vote_counter_data_driven.py
+++ tests/test_vote_counter_data_driven.py
@@ -7,8 +7,15 @@ transitions_path = 'data/transitions.tsv'
 posts_path = 'data/posts/'
 
 dataset = PhaseDataset(archive_path, transitions_path, posts_path)
-data_indices = list(range(len(dataset)))[:1]
+data_indices = list(range(len(dataset)))
 data_labels = dataset.item_labels()
+
+
+def test_phase_dataset():
+    dataset = PhaseDataset(archive_path, transitions_path, posts_path)
+    phase = dataset[1]
+    assert phase['correct'] == ["Substrike22"]
+    assert phase['posts'][0]['number'] == '470'
 
 
 @pytest.mark.parametrize('data_index', data_indices, ids=data_labels)
@@ -28,20 +35,26 @@ def test_vote_extraction(data_index):
     transition_start = 0
     transition_end = 0
     for post in phase['posts']:
+
         if not votecounter.choice:
             # votecounter.process_events(post)
-            votecounter.process_votes(post)
+            votecounter.process_votes(post['user'], post['content'], post['number'])
         elif not transition_start:
             if post['user'] in phase['moderators']:
-                transition_start = post['number']
-        elif not transition_end:
-            if post['user'] in phase['moderators']:
-                transition_end = int(post['number'])
+                transition_start = int(post['number'])
+        elif post['user'] in phase['moderators']:
+            transition_end = int(post['number'])
         else:
             break
 
     if phase['canPredictLynch']: 
+        if votecounter.choice != phase['correct']:
+            for line in votecounter.votecount.votelog:
+                print(line)
         assert votecounter.choice == phase['correct']
     if phase['canPredictTransition']:
-        transition_range = list(range(transition_start, transition_end))
+        transition_range = list(range(transition_start, transition_end+1))
+        if int(phase['phase_end']) not in transition_range:
+            for line in votecounter.votecount.votelog:
+                print(line)
         assert int(phase['phase_end']) in transition_range
